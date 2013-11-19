bb239d04b725c6c1290091a1337e6d0b0676c390
Viktor T <viktortnk@gmail.com>
2013-11-19 22:55:49 +0000

    adjust directory cleanup on running

--- src/test/scala/com/whisk/blackpepper/test/CassandraInst.scala
+++ src/test/scala/com/whisk/blackpepper/test/CassandraInst.scala
@@ -7,11 +7,11 @@ object CassandraInst {
 
   lazy val service: EmbeddedCassandraService = {
     System.setProperty("cassandra.config", getClass.getClassLoader.getResource("cassandra.yaml").toString)
-    //    System.setProperty("cassandra.config", "file:///Users/viktortnk/whisk/blackpepper/src/test/resources/cassandra.yaml")
-    //    println(this.getClass.getClassLoader.getResource("cassandra.yaml").getPath)
     System.setProperty("cassandra-foreground", "yes")
     System.setProperty("log4j.defaultInitOverride", "false")
-    FileUtils.deleteRecursive(new java.io.File("tmp"))
+    val f = new java.io.File("tmp")
+    if (f.exists()) FileUtils.deleteRecursive(f)
+    f.mkdir()
     val s = new EmbeddedCassandraService
     s.start()
     s
