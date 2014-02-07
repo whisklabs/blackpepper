c8100d04a5fc9d77d6dc64e2d625f48f3cbce75d
Viktor T <viktortnk@gmail.com>
2014-02-07 09:26:02 +0000

    ability to specify ttl

--- src/main/scala/com/whisk/blackpepper/UpdateQuery.scala
+++ src/main/scala/com/whisk/blackpepper/UpdateQuery.scala
@@ -1,6 +1,6 @@
 package com.whisk.blackpepper
 
-import com.datastax.driver.core.querybuilder.{ Assignment, Clause, Update }
+import com.datastax.driver.core.querybuilder._
 
 class UpdateQuery[T <: CTable[T, R], R](table: T, val qb: Update) {
 
@@ -26,10 +26,29 @@ class UpdateWhere[T <: CTable[T, R], R](table: T, val qb: Update.Where) {
 
 class AssignmentsQuery[T <: CTable[T, R], R](table: T, val qb: Update.Assignments) extends ExecutableStatement {
 
+  def usingTtl(seconds: Int): AssignmentOptionQuery[T, R] = {
+    new AssignmentOptionQuery[T, R](table, qb.using(QueryBuilder.ttl(seconds)))
+  }
+
+  def using(u: Using): AssignmentOptionQuery[T, R] = {
+    new AssignmentOptionQuery[T, R](table, qb.using(u))
+  }
+
   def modify(a: T => Assignment): AssignmentsQuery[T, R] = {
     new AssignmentsQuery[T, R](table, qb.and(a(table)))
   }
 
   def and = modify _
 
+}
+
+class AssignmentOptionQuery[T <: CTable[T, R], R](table: T, val qb: Update.Options) extends ExecutableStatement {
+
+  def usingTtl(seconds: Int): AssignmentOptionQuery[T, R] = {
+    new AssignmentOptionQuery[T, R](table, qb.and(QueryBuilder.ttl(seconds)))
+  }
+
+  def using(u: Using): AssignmentOptionQuery[T, R] = {
+    new AssignmentOptionQuery[T, R](table, qb.and(u))
+  }
 }
